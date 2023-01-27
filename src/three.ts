08d36836c90fa6a9b519d2e59bd0bc8f7c5508f5
Josue Palomo <josuepalomocs@gmail.com>
2023-01-27 15:16:40 -0600

    add cube spinning animation

--- src/three.ts
+++ src/three.ts
@@ -5,35 +5,37 @@ class BasicCanvas {
   readonly camera;
   private renderer;
 
+  private cube;
+
   constructor() {
     this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera(
-      75,
+      45,
       window.innerWidth / window.innerHeight,
       0.1,
       1000
     );
-    this.camera.position.z = 5;
+    this.camera.position.z = 15;
     this.renderer = new THREE.WebGLRenderer();
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(this.renderer.domElement);
 
-    this.addCube();
-
-    this.render();
+    this.cube = this.createCube();
+    this.scene.add(this.cube);
+    this.renderCube();
   }
 
-  private addCube(): void {
+  private createCube(): THREE.Mesh<THREE.BoxGeometry, THREE.MeshBasicMaterial> {
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     const material = new THREE.MeshBasicMaterial({ color: "#FFFFFF" });
-    const cube = new THREE.Mesh(geometry, material);
-    this.scene.add(cube);
+    return new THREE.Mesh(geometry, material);
   }
 
-  private render(): void {
-    requestAnimationFrame(this.render);
+  private renderCube(): void {
+    requestAnimationFrame(this.renderCube.bind(this));
+    this.cube.rotation.x += 0.008;
+    this.cube.rotation.y += 0.005;
     this.renderer.render(this.scene, this.camera);
   }
 }
-
 const canvas = new BasicCanvas();
